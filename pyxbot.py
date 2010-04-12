8f059d849811d58fc8fd4672a730c8d61f1d9e30
Serge Wroclawski <serge@wroclawski.org>
2010-04-12 09:53:45 -0400

    First commit. Not yet 100% functional

---
+++ pyxbot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+from xml.sax.handler import ContentHandler
+from xml.sax.saxutils import XMLGenerator
+from xml.sax.xmlreader import AttributesNSImpl
+from xml.sax import make_parser
+from datetime import datetime
+
+from pprint import pprint
+
+bounds = (None, None, None, None)
+
+class OSMHandler(ContentHandler):
+    def __init__(self):
+        # ContentHandler.__init__(self)
+        #self._out = out
+        # This is a bad state machine
+        # This is a generic handler for nodes, ways or relations
+        self.clear()
+    def clear(self):
+        self.tags = {}
+        self.ways = []
+        self.members = []
+        self.attrs = {}
+    def startElement(self, name, attrs):
+        if name == 'tag':
+            self.tags[attrs.get('k')] = attrs.get('v')
+        elif name == 'way':
+            self.ways.append(attrs.copy())
+        elif name == 'member':
+            self.members.append(attrs.copy())
+        elif name == 'node' or name == 'way' or name == 'relation':
+            self.attrs = attrs.copy()
+    def endElement(self, name):
+        self.clear()
+
+parser = make_parser()
+parser.setContentHandler(OSMHandler())
+fname = sys.argv[1]
+print fname
+fh = open(fname)
+parser.parse(fh)
+
