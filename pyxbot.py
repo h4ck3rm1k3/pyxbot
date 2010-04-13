0eb71f423b315d57a4b5035f2d051b45f7dc21e5
Serge Wroclawski <serge@wroclawski.org>
2010-04-13 19:50:17 +0200

    Fixing huge bug

--- pyxbot.py
+++ pyxbot.py
@@ -4,65 +4,72 @@ import sys
 from xml.sax.handler import ContentHandler
 from xml.sax import make_parser
 
+BOTNAME = "pyxbot"
+VERSION = "0.1"
+
 class OSMHandler(ContentHandler):
     """This is a base OSMHandler class which sets up the XML parsing, etc.
 
     You will want to override the selectElement and transformElement
     functions"""
-    def __init__(self):
+    def __init__(self, out):
+        self.out = out
         "Initiate the SAX state machine"
         self.clear()
     # These methods are quick n dirty. Very dirty.
-    def _emit_node(self, out):
+    def _emit_node(self):
         "Output a node"
         if self.tags:
-            out.write('<node %s >\n' %
+            self.out.write('<node %s >\n' %
                       ' '.join(['%s="%s"' % (x,y)
                                 for x,y in self.attrs.items()]))
             for tag in self.tags:
-                out.write(u'  <tag k="%s" v="%s" />\n' %
-                          (tag, self.tags[tag]))
-            out.write('</node>\n')
+                self.out.write(u'  <tag k="%s" v="%s" />\n' % (tag, self.tags[tag]))
+            self.out.write('</node>\n')
         else:
-            out.write('<node %s />\n' %
+            self.out.write('<node %s />\n' %
                       ' '.join(['%s="%s"' % (x,y)
                                 for x,y in self.attrs.items()]))
-    def _emit_way(self, out):
+    def _emit_way(self):
         "Output a way"
-        out.write('<way %s >\n' % ' '.join(['%s="%s"' % (x, y)
+        self.out.write('<way %s >\n' % ' '.join(['%s="%s"' % (x, y)
                                             for x, y in self.attrs.items()]))
         if self.tags or self.nodes:
             for nodeid in self.nodes:
-                out.write('  <nd ref="%s" />\n' % nodeid)
+                self.out.write('  <nd ref="%s" />\n' % nodeid)
             for tag in self.tags:
-                out.write('  <tag k="%s" v="%s" />\n' % (tag, self.tags[tag]))
-            out.write('</way>\n')
+                self.out.write('  <tag k="%s" v="%s" />\n'.decode()
+                          % (tag, self.tags[tag]))
+            self.out.write('</way>\n')
         else:
-            out.write('<way %s />\n' %
+            self.out.write('<way %s />\n' %
                       ' '.join(['%s="%s"' % (x,y) for x,y in self.attrs]))
 
-    def _emit_relation(self, out):
+    def _emit_relation(self):
         "Output a relation"
         if self.members or self.tags:
-            out.write('<relation %s >\n' %
+            self.out.write('<relation %s >\n' %
                       ' '.join(['%s="%s"' % (x,y) for x,y in self.attrs]))
             for member in self.members:
-                out.write('  <member %s />\n' %
+                self.out.write('  <member %s />\n' %
                           ' '.join(['%s="%s"' % (x,y)
                                     for x,y in member.items()]))
-            out.write('</relation>\n')
+            for tag in self.tags:
+                self.out.write('  <tag k="%s" v="%s" />\n'.decode()
+                          % (tag, self.tags[tag]))
+            self.out.write('</relation>\n')
         else:
-            out.write('<relation %s />\n' %
+            self.out.write('<relation %s />\n' %
                       ' '.join(['%s="%s"' % (x,y)
                                 for x,y in self.attrs.items()])) 
-    def emit(self, out = sys.stdout):
+    def emit(self):
         "Output the current element"
         if self.name == 'node':
-            self._emit_node(out)
+            self._emit_node()
         elif self.name == 'way':
-            self._emit_way(out)
+            self._emit_way()
         elif self.name == 'relation':
-            self._emit_relation(out)
+            self._emit_relation()
 
     def clear(self):
         "Initialize the state machine"
@@ -88,7 +95,7 @@ class OSMHandler(ContentHandler):
         elif name == 'member':
             self.members.append(attrs.copy())
         elif name == 'nd':
-            self.nodes = attrs.get('ref')
+            self.nodes.append(attrs.get('ref'))
 
     def selectElement(self):
         """Select whether or not we care about the OSM object (True or
@@ -99,24 +106,39 @@ class OSMHandler(ContentHandler):
         """Transform the element. Override this function in your
         handler"""
         pass
+    def modifyElement(self):
+        """Modify this element"""
+        self.out.write('<modify version="%s" generator="%s">\n' %
+                       (VERSION, BOTNAME))
+        self.emit()
+        self.out.write("</modify>\n")
     def deleteElement(self):
         """Returns the string to delete the element.  Please use with
         caution!"""
-        pass
+        self.out.write('<delete version="%s" generator="%s">\n' %
+                       (VERSION, BOTNAME))
+        self.emit()
+        self.out.write('</delete>\n')
     def endElement(self, name):
         """As per the SAX handler, this method is where any work is
         done. You may want to override it, but probably not"""
-        if self.selectElement():
-            self.transformElement()
-            self.emit()
-        self.clear()
+        if name == 'node' or name == 'way' or name == 'relation':
+            if self.selectElement():
+                self.transformElement()
+                self.modifyElement()
+            self.clear()
 
 class PassThroughHandler(OSMHandler):
     def selectElement(self):
         return True
 
+class MyHandler(OSMHandler):
+    def selectElement(self):
+        return self.attrs.get('id') == "55319624"
+
 parser = make_parser()
-parser.setContentHandler(PassThroughHandler())
+parser.setContentHandler(MyHandler(sys.stdout))
 fname = sys.argv[1]
+out = sys.stdout
 fh = open(fname)
 parser.parse(fh)
